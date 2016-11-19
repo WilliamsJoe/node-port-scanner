34214c0b52148b7f7e1423a855dbebe627761b61
williamsjoe <jwj.robert@gmail.com>
2016-11-19 13:44:06 -0500

    Add Connection class

--- server.js
+++ server.js
@@ -2,24 +2,58 @@ require("node-codein");
 
 const net = require('net');
 const host = 'localhost';
-let start = 1;
-const end = 10000;
-const PORT = 8080;
+let PORT = 1;
+const END = 10000;
+// const PORT = 8080;
 
-const socket = net.createConnection(PORT, '127.0.0.1');
-socket.on('connect', function () {
-	console.log('Connected:', PORT);
-	console.log('Socket', socket);
-	// socket.destroy();
-});
+class Connection {
+    constructor(port) {
+        this.port = port
+        this.connection;
+    }
+    connect() {
+        this.connection = net.connect({ port: this.port }, () => {});
+        this.handleConnect();
+        this.handleError();
+    }
+    handleConnect() {
+        this.connection.on('connect', () => {
+            console.log('Connected on:', this.port);
+        });
+    }
+    handleError() {
+    	this.connection.on('error', () => {
+    		return;
+    	});
+    }
+}
 
-socket.on('close', (e) => {
-	console.log('Closed', e);
-});
+for (; PORT <= END; PORT++) {
+    console.log('PORT', PORT);
+    //    try {
+    // 	   net.createConnection(PORT, '127.0.0.1');
+    // } catch(err) {
+    // 	console.log('err')
+    // }
 
-socket.on('data', () => {
-	console.log('data');
-});
+    const client = new Connection(PORT);
+    client.connect();
+
+}
+// const socket = net.createConnection(PORT, '127.0.0.1');
+// socket.on('connect', function() {
+//     console.log('Connected:', PORT);
+//     console.log('Socket', socket);
+//     // socket.destroy();
+// });
+
+// socket.on('close', (e) => {
+//     console.log('Closed', e);
+// });
+
+// socket.on('data', () => {
+//     console.log('data');
+// });
 // const timeout = 2000;
 // console.log('Start');
 // while (start <= end) {
@@ -49,3 +83,5 @@ socket.on('data', () => {
 
 // 	start++;
 // }
+
+
